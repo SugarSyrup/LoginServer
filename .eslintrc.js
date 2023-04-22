8c54c1e876d205081ad746402a57c25854ab813a
SugarSyrup <tlfvm04@naver.com>
2023-04-23 01:49:51 +0900

    Boards

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        useTabs: false,
+      },
+    ],
+    indent: [4, 'tab'],
+    'no-tabs': 0,
   },
 };
